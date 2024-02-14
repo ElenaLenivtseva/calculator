53bd54387f8d9ff5fe7862035752181554ef4054
ElenaLenivtseva <lenivtseva.lena@yandex.ru>
2024-02-14 20:25:35 +0700

    Добавлен компонент операций

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import {useReducer} from 'react'
 import './styles.css';
 import DigitButton from './DigitButton';
+import OperationButton from './DigitButton';
 
 export const ACTIONS = {
   ADD_DIGIT: 'add_digit',
@@ -28,19 +29,19 @@ function App() {
       </div>
       <button className="span-two">AC</button>
       <button>DEL</button>
-      {/* <DigitButton digit='/' dispatch={dispatch}/> */}
+      <OperationButton digit='/' dispatch={dispatch}/>
       <DigitButton digit='1' dispatch={dispatch}/>
       <DigitButton digit='2' dispatch={dispatch}/>
       <DigitButton digit='3' dispatch={dispatch}/>
-      <button>*</button>
+      <OperationButton digit='*' dispatch={dispatch}/>
       <DigitButton digit='4' dispatch={dispatch}/>
       <DigitButton digit='5' dispatch={dispatch}/>
       <DigitButton digit='6' dispatch={dispatch}/>
-      <button>+</button>
+      <OperationButton digit='+' dispatch={dispatch}/>
       <DigitButton digit='7' dispatch={dispatch}/>
       <DigitButton digit='8' dispatch={dispatch}/>
       <DigitButton digit='9' dispatch={dispatch}/>
-      <button>-</button>
+      <OperationButton digit='-' dispatch={dispatch}/>
       <DigitButton digit='.' dispatch={dispatch}/>
       <DigitButton digit='0' dispatch={dispatch}/>
       <button className='span-two'>=</button>
--- src/DigitButton.js
+++ src/DigitButton.js
@@ -1,5 +1,5 @@
 import { ACTIONS } from "./App"
-export default function DigitButton ({dispatch, digit}) {
+export default function OperationButton ({dispatch, digit}) {
     return (
 <button onClick={()=>dispatch({type: ACTIONS.ADD_DIGIT, payload: {digit}})}>{digit}</button>
     )
